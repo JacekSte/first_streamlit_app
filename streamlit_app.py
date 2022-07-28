aa24996b4200c14451065aff2bd414b4a8dc8427
JacekSte <110105959+JacekSte@users.noreply.github.com>
2022-07-28 11:18:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 
 
-streamlit.title('🥕🥝 Built Your Own Fruit Smoothie 🍏🍓')
+streamlit.title('🥕🥝Built Your Own Fruit Smoothie🍏🍓')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
